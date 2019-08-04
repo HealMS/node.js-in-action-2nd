7094507849fb78576f05d6ab7263ad0352e7e971
denislin <HealMSlin@gmail.com>
2019-08-04 16:58:21 +0800

    test

--- chapter06/shoutbox/models/user.js
+++ chapter06/shoutbox/models/user.js
@@ -1,5 +1,6 @@
 const redis = require('redis');
 const db = redis.createClient();
+const saltRound = 10;
 class User {
     constructor(obj) {
         for (let key in obj) {
@@ -16,14 +17,28 @@ class User {
                 this.hashPassword(err => {  //设置加盐密码
                     if (err) return cb(err);
                     this.update(cb);  //设置完密码在回调中向数据库插入数据
-                })
+                });
             });
         }
     }
     update(cb) {
-        
+        const id = this.id;
+        db.set(`user:id:${this.name}`, id, err => {
+            if (err) return cb(err);
+            db.hmset(`user:${id}`, this, err => cb(err));
+        });
     }
     hashPassword(cb) {
-        
+        bcrypt.genSalt(saltRound, (err, salt) => {
+            if (err) return cb(err);
+            this.salt = salt;
+            bcrypt.hash(this.pass, salt, (err, hash) => {
+                if (err) cb(err);
+                this.pass = hash;
+                cb();
+            })
+        })
     }
 }
+
+module.exports = User;
